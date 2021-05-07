c8e5665e54b9ca2db83d93d877a80e3346f60409
Stefan Frei <stefan.a.frei@gmail.com>
2021-05-07 11:28:45 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Search msg="Hello Vue 3 + Vite" />
+</template>
+
+<script setup>
+import Search from './components/Search.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+body {
+  background-color:#2c3e50;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
